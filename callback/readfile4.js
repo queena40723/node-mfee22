b02311a81d447d228085f51d7bd2597c9a7caa5a
Queena Tao <queena113@gmail.com>
2022-01-09 10:36:54 +0800

    add error description

--- callback/readfile4.js
+++ callback/readfile4.js
@@ -1,6 +1,6 @@
 const { readFile } = require("fs");
 let readfilePromise = new Promise((resolve,reject)=>{
-    readFile("text.txt", "utf-8", (err, data) => {
+    readFile("test.txt", "utf-8", (err, data) => {
         if (err) {
           reject(err);
           return;
@@ -9,30 +9,36 @@ let readfilePromise = new Promise((resolve,reject)=>{
       });
     })
 //例一
-async function main(){
-    try {
-        let result1 = await readfilePromise
-        console.log(result1)
-    }
-    catch(err){
-        console.log('catch one error', err);
-    }
+// async function main(){
+//     try {
+//         let result1 = await readfilePromise
+//         console.log(result1)
+//     }
+//     catch(err){
+//         console.log('catch one error', err);
+//     }
         
-}
+// }
 //例二
 async function main(){
-    let result1 = await readfilePromise
+    console.log('123') //123
+
+    let result1 = await readfilePromise //error產生要抓他，不然後面的程式 會崩潰
+    console.log('456') //不出現 後續都不會產生
+
     console.log(result1)
+    console.log('123')
+
 }
 main();  
 
 //IFEE
-(async()=>{
-    try {
-        let result1 = await readfilePromise
-        console.log(result1)
-    }
-    catch(err){
-        console.log('catch one error', err);
-    }
-})();
+// (async()=>{
+//     try {
+//         let result1 = await readfilePromise
+//         console.log(result1)
+//     }
+//     catch(err){
+//         console.log('catch one error', err);
+//     }
+// })();
